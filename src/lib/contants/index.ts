ba7c08e2b8da53c00714695dd902fc0e4a0e9b9a
Josh <joshua.l.mertens@gmail.com>
2021-12-19 17:04:04 +0000

    add helpers and constants files

---
+++ src/lib/contants/index.ts
@@ -0,0 +1,12 @@
+export const contact = {
+	phone: '0438833354',
+	email: 'brad@bradcannre.com.au'
+};
+
+// const hoursOfOperation = {};
+
+export const links = {
+	phone: `tel:${contact.phone}`,
+	facebook: 'https://www.facebook.com/Brad-Cann-Real-Estate-106718608345609',
+	email: `mailto:${contact.email}`
+};
