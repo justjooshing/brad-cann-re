0c88478436afa3b683d0dc09946ef29ded112c12
Josh <joshua.l.mertens@gmail.com>
2022-01-06 19:09:46 +0000

    Add htags to appraisalsBanner

--- src/lib/components/AppraisalsBanner.svelte
+++ src/lib/components/AppraisalsBanner.svelte
@@ -8,11 +8,13 @@
 	<img src={chelsea_pier} alt="chelsea beach pier" />
 	<div class="bg">
 		<div class="content">
-			<Htag size={1} style={3} message="Work with someone you can trust" />
-			<p>
-				Looking for an agent that is focused on your success? Call us today, we are ready to listen.
-			</p>
-			<p>Request an appraisal today</p>
+			<Htag size={1} style={1} message="Work with someone you can trust" />
+			<Htag
+				size={2}
+				style={5}
+				message="Looking for an agent that is focused on your success? Call us today, we are ready to listen."
+			/>
+			<Htag size={2} style={5} message="Request an appraisal today" />
 			<div class="buttons">
 				<Button href="/sales">SALES APPRAISAL</Button>
 				<Button href="/rentals">RENTAL APPRAISAL</Button>
@@ -54,6 +56,7 @@
 
 				.buttons {
 					display: flex;
+					width: max-content;
 				}
 			}
 		}
