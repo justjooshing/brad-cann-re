4c5066085a83a03278828cad62934497d58f055a
Josh <joshua.l.mertens@gmail.com>
2022-01-06 20:19:02 +0000

    Add basic HelpingYou file

--- src/lib/components/HelpingYou.svelte
+++ src/lib/components/HelpingYou.svelte
@@ -1,13 +1,58 @@
-<script>
-	// your script goes here
+<script lang="ts">
+	import { contact, contactLinks } from '$lib/contants/index.svelte';
+	import Button from './Button.svelte';
+	import ContentWrap from './ContentWrap.svelte';
+	import Htag from './Htag.svelte';
+	import Icon from './Icon.svelte';
 </script>
 
-<!-- 
-	Helping you is our passion
-		People image
-		Text
-		CTA Call Now button
- -->
-<style>
-	/* your styles go here */
+<div class="wrapper">
+	<img src="" alt="people" />
+	<ContentWrap>
+		<Htag centered size={1} style={2} message="Helping you is our passion" />
+		<p>Navigating all areas of the real estate world can be challenging at the best of times.</p>
+		<p>
+			Whether your trying to find your first home, selling your current property, curious what your
+			property might be worth or property management solutions.
+		</p>
+		<p>
+			Take away the stress, worry and confusion and call Brad Cann and Partners, with over 22 years
+			of real estate experience we can help you make the right choice easy. No strings attached.
+		</p>
+		<p>
+			Brad Cann and Partners opened in 2001 and has now moved its operations to a fully online real
+			estate model in partnership with ExP Australia, a better more cost effective solution to real
+			estate sales for both agents and customers.
+		</p>
+		<Button href={contactLinks.phone}>
+			<div class="button">
+				<Icon alt icon="phone" />
+				<span>Call us now: {contact.phone}</span>
+			</div>
+		</Button>
+	</ContentWrap>
+</div>
+
+<style lang="scss">
+	@import '../global/index.scss';
+
+	.wrapper {
+		display: grid;
+		grid-template-rows: 1fr 1fr;
+		@include medium {
+			grid-template-columns: 1fr 1fr;
+		}
+	}
+	p {
+		color: #74746a;
+	}
+	span {
+		font-size: $text-standard;
+		margin-left: 15px;
+	}
+	.button {
+		display: flex;
+		place-content: center;
+		place-items: center;
+	}
 </style>
