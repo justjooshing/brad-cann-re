6be5c4a5dcd2039335dedac8f6fed1513a6fff1a
Josh <joshua.l.mertens@gmail.com>
2022-02-26 19:37:33 +0000

    Add sliding background pier image

--- src/lib/components/AppraisalsBanner.svelte
+++ src/lib/components/AppraisalsBanner.svelte
@@ -1,13 +1,12 @@
 <script>
-	import chelsea_pier from '$assets/chelsea_pier.webp';
+	import BackgroundPier from './BackgroundPier.svelte';
 	import Button from './Button.svelte';
 	import ContentWrap from './ContentWrap.svelte';
 	import Htag from './Htag.svelte';
 </script>
 
-<div class="appraisals_wrapper">
-	<img src={chelsea_pier} alt="chelsea beach pier" />
-	<ContentWrap>
+<ContentWrap>
+	<div class="appraisals_wrapper">
 		<div class="content">
 			<Htag size={1} style={1} message="Work with someone you can trust" />
 			<Htag
@@ -21,21 +20,19 @@
 				<Button href="/rentals">RENTAL APPRAISAL</Button>
 			</div>
 		</div>
-	</ContentWrap>
-</div>
+		<BackgroundPier />
+	</div>
+</ContentWrap>
 
 <style lang="scss">
 	@import '../global/index.scss';
 
 	.appraisals_wrapper {
 		position: relative;
-		display: flex;
-		flex-direction: row;
-		img {
-			width: 50%;
-			object-position: 20% 33%;
-			object-fit: cover;
-		}
+		display: grid;
+		grid-template-rows: auto 1fr;
+		grid-gap: 1em;
+		place-items: center;
 
 		.content {
 			display: flex;
@@ -44,12 +41,8 @@
 			text-shadow: 1px 1px 3px black;
 
 			.buttons {
-				display: grid;
-				grid-gap: 5px;
-				grid-template-rows: 1fr 1fr;
-				@include medium {
-					grid-template-columns: 1fr 1fr;
-				}
+				display: flex;
+				place-content: space-around;
 			}
 		}
 	}
---
+++ src/lib/components/BackgroundPier.svelte
@@ -0,0 +1,20 @@
+<script>
+</script>
+
+<div class="image" />
+
+<style lang="scss">
+	@import '../global/index.scss';
+
+	.image {
+		padding: 8em 0;
+		width: 100%;
+		background: url('$assets/chelsea_pier.webp') transparent no-repeat;
+		background-size: contain;
+
+		@include medium {
+			background-attachment: fixed;
+			background-size: cover;
+		}
+	}
+</style>
