cd3d6819ba8f2f32546c9d81ae23c05f7e8721fb
Josh <joshua.l.mertens@gmail.com>
2022-02-26 17:49:36 +0000

    Add bg colours to ContentWrap

--- src/lib/components/ContentWrap.svelte
+++ src/lib/components/ContentWrap.svelte
@@ -1,9 +1,30 @@
-<section class="padding">
+<script lang="ts">
+	export let bgColor: 'bgPrimary' | 'bgSecondary' | 'bgTertiary' = 'bgPrimary';
+</script>
+
+<section
+	class="padding"
+	class:bgPrimary={bgColor === 'bgPrimary'}
+	class:bgSecondary={bgColor === 'bgSecondary'}
+	class:bgTertiary={bgColor === 'bgTertiary'}
+>
 	<slot />
 </section>
 
-<style>
+<style lang="scss">
+	@import '../global/index.scss';
+
 	.padding {
 		padding: 1em;
+	}
+
+	.bgPrimary {
+		background: $bg-primary;
+	}
+	.bgSecondary {
+		background: $bg-secondary;
+	}
+	.bgTertiary {
+		background: $bg-tertiary;
 	}
 </style>
